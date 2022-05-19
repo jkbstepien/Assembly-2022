90b175bccf6808e12af2b075af31fb8e080c2ac4
jkbstepien <devjakub.stepien@gmail.com>
2022-05-19 17:42:19 +0200

    ex 1 - 80%

--- zad1.asm
+++ zad1.asm
@@ -7,15 +7,18 @@ stack1 ends
 
 data1 segment
 	buffer		db	200 dup('$')
-	text_ask_a	db	"Input a (range: 0-8): $"
-	text_ask_b	db	"Input b (range: 0-8): $"
+	text_ask_a	db	"Input a (range: 1-8): $"
+	text_ask_b	db	"Input b (range: 1-8): $"
 	text_result_x	db	"f(x) = 0 when x = $"
 	text_result_y	db	"f(0) = $"
+	text_minus_sign	db	"-$"
+	text_test	db	"TEST", 13, 10, '$'
 	nl		db	13, 10, '$'
-	valueA		dw	?
-	valueB		dw	?
+	valueA		dw	?, '$'
+	valueB		dw	?, '$'
 	root		db	?
-	axis_size	db	20
+	ctr_in		dw	?
+	text_plot_scale	db	13, 10, "One character equals 1 unit.$"
 data1 ends
 
 code1 segment
@@ -24,11 +27,6 @@ start1:
 	mov	ax, seg stack1		; initialize stack
 	mov	ss, ax
 	mov	sp, offset ws1
-
-	call	process_input
-
-	mov	ax, 4c00h		; end program
-	int	21h
 
 process_input:
 	call	get_ab
@@ -39,29 +37,94 @@ process_input:
 	cbw
 	idiv	bl			; ah - remainder, al = result
 
-	add	al, 48d
-	call	print_regs
-
-	mov	dx, seg buffer
-	mov	es, dx
-	mov	di, offset buffer
-
-	mov	byte ptr es:[di], al
-	mov	byte ptr es:[di + 1], 13d
-	mov	byte ptr es:[di + 2], 10d
-	mov	byte ptr es:[di + 3], '$'
-	
-	mov	dx, offset buffer
+	mov	dx, offset text_result_x ; print answer for f(x)=0
 	call	base_print
 	
-	mov	dx, offset text_result_x
+	cmp	al, 0
+	jge	nonnegative
+
+	mov	dx, offset text_minus_sign
 	call	base_print
+
+	neg	al
+;	xchg	ax, bx
+
+nonnegative:
+	add	ax, 48d
+	mov	[ds:valueA], ax
+
+	mov	dx, offset valueA
+	call	base_print
+
+	mov	dx, offset nl		; print answer for y when x=0
+	call	base_print
+	mov	dx, offset text_result_y
+	call	base_print
+	mov	dx, offset valueA
+	call	base_print
+
+	sub	ax, 48d
+	mov	[ds:valueA], ax
 
 	mov	dx, offset nl
 	call	base_print
 
-	call	print_regs
-	ret
+plot:
+	mov	dx, seg buffer
+	mov	es, dx
+	mov	di, offset buffer
+
+	mov	byte ptr es:[di + 1], '$'
+
+	mov	cx, 20d
+	loop1:
+		push	cx
+		mov	[ds:ctr_in], cx
+		mov	cx, 20d
+
+		loop2:
+			mov	byte ptr es:[di], ' '
+			check0:
+				cmp	cx, 10d
+				jne	check1
+				mov	byte ptr es:[di], '#'
+			check1:
+				cmp	[ds:ctr_in], 10d
+				jne	check2
+				mov	byte ptr es:[di], '#'
+			check2:
+				xor	ax, ax
+				xor	bx, bx
+
+				mov	ax, 10d
+				mov	bx, [ds:valueA]
+				sub	ax, cx
+				imul	bl
+				mov	bx, [ds:valueB]
+				add	ax, bx
+				sub	ax, [ds:ctr_in]
+				add	ax, 10d
+
+				cmp	ax, 0
+				jne	end_loop2
+				mov	byte ptr es:[di], '*'
+
+			end_loop2:
+				mov	dx, offset buffer
+				call	base_print
+				loop	loop2
+
+		mov	dx, offset nl
+		call	base_print
+		pop	cx
+		loop	loop1
+
+end1:
+	mov	dx, offset text_plot_scale
+	call	base_print
+
+	mov	ax, 4c00h
+	int	21h
 
 get_ab:
 	mov 	dx, offset text_ask_a	; prompt user for a value
@@ -96,13 +159,17 @@ get_ab:
 	ret
 
 base_print:
+	push	ax
 	mov	ax, seg data1
 	mov	ds, ax
 	mov	ah, 9
 	int	21h
+	pop	ax
 	ret
 
 print_regs:
+    mov dx, offset nl
+    call base_print
     ; save registers
     push ax
     push bx
